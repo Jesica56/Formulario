d711aea709d9a192c7e4633c8c2243ea2f1e68ae
Jesica56 <martinezjesicanoelia56@gmail.com>
2023-11-02 10:08:02 -0300

    Subiendo ejercisio resuelto

---
+++ form/js/form.js
@@ -0,0 +1,5 @@
+function capturarEstado() {
+    const estadoSelect = document.getElementById("estado");
+    const estadoSeleccionado = estadoSelect.value;
+    alert("Estado seleccionado: " + estadoSeleccionado);
+}
